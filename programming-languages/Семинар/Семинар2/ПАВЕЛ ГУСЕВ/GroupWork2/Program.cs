5c022825f67c0619f25306275d31e02ab726b7ae
ZloDoc <zlodoc@rambler.ru>
2022-07-15 18:05:54 +0300

    GroupWork2 witg Pavel completed

--- programming-languages/Семинар/Семинар2/ПАВЕЛ ГУСЕВ/GroupWork2/Program.cs
+++ programming-languages/Семинар/Семинар2/ПАВЕЛ ГУСЕВ/GroupWork2/Program.cs
@@ -15,4 +15,12 @@
 
 Console.WriteLine("Введите число :");
 int number = Convert.ToInt32(Console.ReadLine());
-IsKratno(Console.WriteLine());
+bool result = IsKratno(number);
+if (result == true)
+{
+Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно 7 и 23");
+}
