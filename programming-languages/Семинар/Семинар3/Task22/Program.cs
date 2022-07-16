e49a3345e66e55857dfbd5a7af4266b439313525
ZloDoc <zlodoc@rambler.ru>
2022-07-16 20:32:57 +0300

    in process task22

--- programming-languages/Семинар/Семинар3/Task22/Program.cs
+++ programming-languages/Семинар/Семинар3/Task22/Program.cs
@@ -3,3 +3,21 @@
 // квадратов чисел от 1 до N.
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
+
+void SquareTable(int num)
+{
+
+    int[] square=new int[num];
+    for (int index = 0; index <= num; index++)
+    {
+        square[index]=index*index;
+        //Console.WriteLine(index);
+        Console.WriteLine(square[index]);
+
+    }
+
+}
+
+Console.Write("Imput N :");
+int number=Convert.ToInt32(Console.ReadLine());
+SquareTable(number+1);
