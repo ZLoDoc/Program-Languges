bfb4efe4ac398174c30e66fab655a0211b69cb04
ZloDoc <zlodoc@rambler.ru>
2022-07-18 18:44:33 +0300

    Homework for Workshop3 Completed

--- Homework/Семинар3/Task21/Program.cs
+++ Homework/Семинар3/Task21/Program.cs
@@ -2,3 +2,33 @@
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Dist(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double x = x2 - x1;
+    double y = y2 - y1;
+    double z = z2 - z1;
+    double result = Math.Sqrt((x * x) + (y * y) + (z * z));
+    return result;
+}
+
+Console.Write("Введите X1= ");
+double a1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Y1= ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Z1= ");
+double c1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите X2= ");
+double a2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Y2= ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Z2= ");
+double c2 = Convert.ToDouble(Console.ReadLine());
+
+double result = Dist(a1, b1, c1, a2, b2, c2);
+Console.WriteLine(result);
--- Homework/Семинар3/Task23/Program.cs
+++ Homework/Семинар3/Task23/Program.cs
@@ -2,3 +2,20 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+int[] Cube(int a)
+{
+    int[] array = new int[a];
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = index * index * index;
+        //Console.WriteLine(array[index]);
+    }
+    return array;
+}
+
+Console.Write("Введите число :");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = Cube(n);
+foreach ( int index in array ) Console.Write($" {index}");
+
