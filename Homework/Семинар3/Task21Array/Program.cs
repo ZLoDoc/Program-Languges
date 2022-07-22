e52eb6c818a2a19015cf7526079548bb23e46fb8
ZloDoc <zlodoc@rambler.ru>
2022-07-22 18:05:29 +0300

    N-Mass in process

--- Homework/Семинар3/Task21Array/Program.cs
+++ Homework/Семинар3/Task21Array/Program.cs
@@ -1,28 +1,42 @@
-﻿double Distance(int[,] array)
+﻿void Distance(int[,] array, int n)
 {
-    double x = array[1, 0] - array[0, 0];
-    double y = array[1, 1] - array[0, 1];
-    double z = array[1, 2] - array[0, 2];
-    double result = Math.Sqrt((x * x) + (y * y) + (z * z));
-    return result;
-}
-int[,] coordinat = new int[2, 3];
-for (int i = 0; i <= coordinat.GetLength(0) - 1; i++)
-{
-    for (int j = 0; j <= coordinat.GetLength(1) - 1; j++)
+    double result = 0;
+    int diff = array[0, 0];
+    int[] mass = new int[n];
+
+    for (int b = 0; b <= n - 1; b++)
     {
-        if (i == 0)
+        for (int a = 1; a < 2; a++)
         {
-            Console.Write($"Введите {j + 1} координату первой точки : ");
-            coordinat[i, j] = Convert.ToInt32(Console.ReadLine());
+            diff = array[a, b] - diff;
+            Console.WriteLine($"DIFF = {diff}");
+            Console.WriteLine($"ARRAY 2 {a},{b} = {array[a, b]}");
+            //double result = Math.Sqrt((x * x) + (y * y) + (z * z));
+
         }
-        if (i != 0)
-        {
-            Console.Write($"Введите {j + 1} координату второй точки : ");
-            coordinat[i, j] = Convert.ToInt32(Console.ReadLine());
-        }
+
     }
 }
-Console.Write(" ");
-double result = Distance(coordinat);
-Console.WriteLine($"Расстояние между точками {result}");
+Console.WriteLine("Введите размерность пространства в котором находятся точки A и B : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] coordinat = new int[2, n];
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j <= n - 1; j++)
+    {
+        Console.WriteLine($"Введите {j + 1} координату {i + 1} точки : ");
+        coordinat[i, j] = Convert.ToInt32(Console.ReadLine());
+
+    }
+}
+
+// for (int a = 0; a < 2; a++)
+// {
+//     for (int b = 0; b <= n - 1; b++)
+//     {
+//         Console.WriteLine($"ARRAY {a},{b} = {coordinat[a, b]}");
+//     }
+// }
+Distance(coordinat, n);
+
