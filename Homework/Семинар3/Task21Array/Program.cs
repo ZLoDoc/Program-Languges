f28702ada5c77ddc0e1fb4341a6fa64ec6c13434
ZLoDoc <zlodoc@rambler.ru>
2022-07-23 03:12:39 +0300

    Task hard difficult about finding a distance in the two points COMPLETED !!!

--- Homework/Семинар3/Task21Array/Program.cs
+++ Homework/Семинар3/Task21Array/Program.cs
@@ -1,42 +1,43 @@
-﻿void Distance(int[,] array, int n)
+﻿double Distance(int[,] array, int n)
 {
     double result = 0;
     int diff = array[0, 0];
     int[] mass = new int[n];
-
+    int temp0 = 0;
+    int temp1 = 0;
+    int sum = 0;
     for (int b = 0; b <= n - 1; b++)
     {
-        for (int a = 1; a < 2; a++)
+        for (int a = 1; a >= 0; a--)
         {
-            diff = array[a, b] - diff;
-            Console.WriteLine($"DIFF = {diff}");
-            Console.WriteLine($"ARRAY 2 {a},{b} = {array[a, b]}");
-            //double result = Math.Sqrt((x * x) + (y * y) + (z * z));
-
+            if (a == 1)
+            {
+                temp1 = array[a, b];
+            }
+            if (a == 0)
+            {
+                temp0 = array[a, b];
+            }
         }
-
+        mass[b] = temp1 - temp0;
     }
+    for (int count = 0; count <= n - 1; count++)
+    {
+        sum = sum + (mass[count] * mass[count]);
+    }
+    return result = Math.Sqrt(sum);
 }
-Console.WriteLine("Введите размерность пространства в котором находятся точки A и B : ");
+Console.Write("Введите размерность пространства в котором находятся точки A и B : ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[,] coordinat = new int[2, n];
-
 for (int i = 0; i < 2; i++)
 {
     for (int j = 0; j <= n - 1; j++)
     {
         Console.WriteLine($"Введите {j + 1} координату {i + 1} точки : ");
         coordinat[i, j] = Convert.ToInt32(Console.ReadLine());
-
     }
 }
-
-// for (int a = 0; a < 2; a++)
-// {
-//     for (int b = 0; b <= n - 1; b++)
-//     {
-//         Console.WriteLine($"ARRAY {a},{b} = {coordinat[a, b]}");
-//     }
-// }
-Distance(coordinat, n);
+double result = Distance(coordinat, n);
+Console.WriteLine($"Расстояние между точками A и B в {n}-мерном пространстве  {result}");
 
