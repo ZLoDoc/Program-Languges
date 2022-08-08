0c4098ce08fc9834b01d9f8e4210e1a99d0cbfc7
ZloDoc <zlodoc@rambler.ru>
2022-08-08 11:51:44 +0300

    TAsk29

--- Homework/Семинар4/Task29/Program.cs
+++ Homework/Семинар4/Task29/Program.cs
@@ -7,29 +7,53 @@
 int[] Sum(string text)
 {
     int length = text.Length;
-    int[] array = new int[length];
-    var arr = "text".Split(',');
+    int[] array = new int[length / 2];
     char comma = ',';
-    char space = ' ';
     int j = 0;
     string result = String.Empty;
 
     for (int i = 0; i < length; i++)
     {
-        if (text[i] == comma || text[i] == space)
+        if(length==1)
+        {
+            array[j] = Convert.ToInt32(result);
+
+            break;
+        }
+        if (text[i] == comma)
         {
             array[j] = Convert.ToInt32(result);
             result = String.Empty;
             j++;
         }
-        else if (text[i] != comma && text[i] != space) result = result + text[i];
+        else if (text[i] != comma) result = result + text[i];
     }
     array[j] = Convert.ToInt32(result);
-    return (array);
+
+
+                                    Console.WriteLine($"array[j]={array[j]}");
+    int[] resArr = new int[j+1];
+                                    Console.WriteLine($"j={j}");
+                                    Console.WriteLine($"resArr.Lenght={resArr.Length}");
+
+    for (int k = 0; k <= j; k++)
+    {
+                                    Console.WriteLine($"k={k}");
+        resArr[k] = array[k];
+                                    //Console.WriteLine(resArr[j-1]);
+    }
+    return (resArr);
 }
+
+
+
 void PrintArray(int[] array)
 {
-    for (int i = 0; i < array.Length; i++) Console.Write($" {array[i]}");
+    for (int i = 0; i < array.Length; i++)
+    {
+         Console.Write($" {array[i]}");
+         if(i<array.Length-1) Console.Write(",");
+    }
 }
 Console.Write("Введите ряд чисел через запятую :");
 string? text = Console.ReadLine();
