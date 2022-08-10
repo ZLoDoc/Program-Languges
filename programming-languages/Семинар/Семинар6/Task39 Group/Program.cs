49603400c28bf414b1da495da281533fcf46ec4f
ZLoDoc <zlodoc@rambler.ru>
2022-08-10 22:27:42 +0300

    WorkShop6

---
+++ programming-languages/Семинар/Семинар6/Task39 Group/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+Console.WriteLine("введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] massive = new int[size];
+
+for(int i = 0; i < massive.Length; i++)
+    {
+        massive[i] = new Random().Next(0,100);
+    }
+
+PrintArray(massive);
+Console.WriteLine();
+
+    int value=0;
+   
+    for(int i = 0; i < massive.Length/2; i++)
+    {
+        value=massive[i];
+        massive[i]=massive[massive.Length-1-i];
+        massive[massive.Length-1-i] =value;
+    }
+
+PrintArray(massive);
---
+++ programming-languages/Семинар/Семинар6/Task39/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] RndArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i <= array.Length - 1; i++)
+        array[i] = new Random().Next(1, 11);
+        Console.WriteLine($"array{i}={array[i]},");
+    return array;
+}
+
+// int[] Reverse(int[] array)
+// {
+//     int[] result = new int[array.Length];
+//     for (int i = 0; i <= array.Length - 1; i++)
+//     {
+//         result[i] = array.Length - 1 - i;
+//         Console.WriteLine(result[i]);
+//         return result;
+//     }
+// }
+Console.WriteLine("Введите длину массива");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = RndArray(num);
+//int[] res = Reverse(array);
