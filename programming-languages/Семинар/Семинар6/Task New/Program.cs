c8ec26a550847a06009c93df85cffc4cb1d7c137
ZloDoc <zlodoc@rambler.ru>
2022-08-12 17:36:04 +0300

    Task New for WorkShop6 Completed

--- programming-languages/Семинар/Семинар6/Task New/Program.cs
+++ programming-languages/Семинар/Семинар6/Task New/Program.cs
@@ -9,23 +9,44 @@ int[,] RndArray(int str, int col)
     {
         for (int columns = 0; columns < array.GetLength(1); columns++)
         {
-            array[rows, columns] = new Random().Next(0, 10);
+            array[rows, columns] = new Random().Next(10, 100);
         }
     }
     return array;
 }
-void MinMax(int [,]array)
+void MinMax(int[,] array)
 {
-    int min=0;
-    int
+    double min = array[0,0];
+    double max = array[0,0];
+    int minRows = 0;
+    int maxRows = 0;
+    int minColumns = 0;
+    int maxColumns = 0;
+
     for (int rows = 0; rows < array.GetLength(0); rows++)
     {
         for (int columns = 0; columns < array.GetLength(1); columns++)
         {
-            array[rows, columns] = new Random().Next(0, 10);
+            if (max < array[rows, columns])
+            {
+                max = array[rows, columns];
+                maxRows = rows;
+                maxColumns = columns;
+            }
+            if (min > array[rows, columns])
+            {
+                min = array[rows, columns];
+                minRows = rows;
+                minColumns = columns;
+            }
         }
     }
-   }
+    double middle = (min + max) / 2;
+    Console.WriteLine($"Минимальное значение {min} находится в ячейке [{minRows},{minColumns}]");
+    Console.WriteLine($"Максимальное значение {max} находится в ячейке [{maxRows},{maxColumns}]");
+    Console.WriteLine($"Среднее арифметическое максимального {max} и минимального значения {min} = {middle}");
+}
+
 void PrintArray(int[,] array)
 {
     for (int rows = 0; rows < array.GetLength(0); rows++)
@@ -41,5 +62,7 @@ Console.Write("Количество строк : ");
 int str = Convert.ToInt32(Console.ReadLine());
 Console.Write("Количество столбцов : ");
 int col = Convert.ToInt32(Console.ReadLine());
-int[,]array=(RndArray(str, col));
+int[,] array = (RndArray(str, col));
 PrintArray(array);
+MinMax(array);
+
