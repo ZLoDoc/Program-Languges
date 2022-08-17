6fc54915669c298154f7e7d6ecb757f07dd7749f
ZLoDoc <zlodoc@rambler.ru>
2022-08-17 22:19:50 +0300

    Task 60 in progress

--- Homework/Семинар8/Task58/Program.cs
+++ Homework/Семинар8/Task58/Program.cs
@@ -15,3 +15,68 @@
 // 20 81 8 6
 // 56 8 4 24
 // 10 6 24 49
+
+int[,] CreateRndArray1(int num1, int num2)
+{
+    int[,] array = new int[num1, num2];
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            array[rows, columns] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+int[,] CreateRndArray2(int num1, int num2)
+{
+    int[,] array = new int[num1, num2];
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            array[rows, columns] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,]MultipleArray(int[,] array1,int[,]array2)
+{
+    int [,]multArray=new int[array1.GetLength(0),array1.GetLength(1)];
+    for (int rows = 0; rows < multArray.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < multArray.GetLength(1); columns++)
+        {
+            multArray[rows,columns]=array1[rows,columns]*array2[rows,columns];
+        }
+        
+    }
+    return multArray;
+}
+
+Console.Write("Количество строк :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов :");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] array1 = CreateRndArray1(a, b);
+int[,] array2 = CreateRndArray2(a, b);
+Console.WriteLine();
+PrintArray(array1);
+Console.WriteLine();
+PrintArray(array2);
+Console.WriteLine("Произведение двух массивов :");
+PrintArray(MultipleArray(array1,array2));
--- Homework/Семинар8/Task60/Program.cs
+++ Homework/Семинар8/Task60/Program.cs
@@ -5,3 +5,35 @@
 // массив размером 2 x 2 x 2
 // 12(0,0,0) 22(0,0,1)
 // 45(1,0,0) 53(1,0,1)
+
+int[,] CreateRndArray(int num1, int num2)
+{
+    int[,] array = new int[num1, num2];
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            array[rows, columns] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Количество строк :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов :");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] array = CreateRndArray(a, b);
+PrintArray(array);
