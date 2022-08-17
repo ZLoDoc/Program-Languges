37a7ffdbb8de6af0f1d6b24782738d49e217ba37
ZloDoc <zlodoc@rambler.ru>
2022-08-17 18:21:25 +0300

    Task 54 completed

--- Homework/Семинар8/Task54/Program.cs
+++ Homework/Семинар8/Task54/Program.cs
@@ -8,3 +8,61 @@
 // 1 2 4 7
 // 2 3 5 9
 // 2 4 4 8
+
+int[,] CreateRndArray(int num1, int num2)
+{
+    int[,] array = new int[num1, num2];
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            array[rows, columns] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] OrderArray(int[,] array)
+{
+    int temp = 0;
+    //int min=0;
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(1) - 1; j++)
+                {
+                    if (array[rows, j] > array[rows, j + 1])
+                    {
+                        temp = array[rows, j + 1];
+                        array[rows, j + 1] = array[rows, j];
+                        array[rows, j] = temp;
+                    }
+                }
+            }
+        }
+    }
+    return array;
+}
+
+Console.Write("Количество строк :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов :");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] array = CreateRndArray(a, b);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(OrderArray(array));
