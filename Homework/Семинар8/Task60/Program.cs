70120c22abe9ffbe0c37ae7b715117756eefbe4e
ZloDoc <zlodoc@rambler.ru>
2022-08-18 14:49:45 +0300

    Task60 Completed

--- Homework/Семинар8/Task60/Program.cs
+++ Homework/Семинар8/Task60/Program.cs
@@ -6,26 +6,58 @@
 // 12(0,0,0) 22(0,0,1)
 // 45(1,0,0) 53(1,0,1)
 
-int[,] CreateRndArray(int num1, int num2)
+int[,,] CreateRndDifferentNumbersArray(int num1, int num2, int num3)
 {
-    int[,] array = new int[num1, num2];
-    for (int rows = 0; rows < array.GetLength(0); rows++)
+    int[] RandomNumsArray(int length)
     {
-        for (int columns = 0; columns < array.GetLength(1); columns++)
+        int[] tempArray = new int[length];
+        for (int i = 0; i <= tempArray.Length - 1;)
         {
-            array[rows, columns] = new Random().Next(0, 10);
+            int count = 0;
+            int temp = new Random().Next(10, 100);
+            for (int j = 0; j <= tempArray.Length - 1; j++)
+            {
+                if (tempArray[j] == temp) count++;
+            }
+            if (count == 0)
+            {
+                tempArray[i] = temp;
+                i++;
+                count = 0;
+            }
         }
+        return tempArray;
     }
-    return array;
-}
-void PrintArray(int[,] array)
-{
+
+    int[,,] array = new int[num1, num2, num3];
+    int callIndex = 0;
+    int[] tempArray = RandomNumsArray(num1 * num2 * num3);
 
     for (int rows = 0; rows < array.GetLength(0); rows++)
     {
         for (int columns = 0; columns < array.GetLength(1); columns++)
         {
-            Console.Write($"{array[rows, columns]} ");
+            for (int depth = 0; depth < array.GetLength(2); depth++)
+            {
+                array[rows, columns, depth] = tempArray[callIndex];
+                callIndex++;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            for (int depth = 0; depth < array.GetLength(2); depth++)
+            {
+                Console.Write($"{array[rows, columns, depth]}({rows},{columns},{depth}) ");
+            }
+            Console.WriteLine();
         }
         Console.WriteLine();
     }
@@ -35,5 +67,7 @@ Console.Write("Количество строк :");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Количество столбцов :");
 int b = Convert.ToInt32(Console.ReadLine());
-int[,] array = CreateRndArray(a, b);
+Console.Write("задайте глубину массива :");
+int c = Convert.ToInt32(Console.ReadLine());
+int[,,] array = CreateRndDifferentNumbersArray(a, b, c);
 PrintArray(array);
---
+++ Temp/temp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] tempArray = new int[5];
+for (int i = 0; i <= tempArray.Length - 1;)
+{
+    int count = 0;
+    int temp = new Random().Next(10, 19);
+    for (int j = 0; j <= tempArray.Length - 1; j++)
+    {
+        if (tempArray[j] == temp) count++;
+    }
+    if (count == 0)
+    {
+        tempArray[i] = temp;
+        i++;
+        count = 0;
+    }
+}
+for (int n = 0; n <= tempArray.Length - 1; n++) Console.WriteLine(tempArray[n]);
+
+
