57b8b4615d0b1cbeaba9206f5e1b0b52a5782076
ZloDoc <zlodoc@rambler.ru>
2022-08-18 17:57:20 +0300

    Lecture7 in Progress

--- Homework/Семинар8/Task62/Program.cs
+++ Homework/Семинар8/Task62/Program.cs
@@ -4,3 +4,40 @@
 // 12 13 14 5
 // 11 16 15 6
 // 10 9 8 7
+
+int[,] array = new int[4, 4];
+const int n = 4;
+const int m = 4;
+// int[,] matrix = new int[n, m];
+
+int row = 0;
+int col = 0;
+int dx = 1;
+int dy = 0;
+int dirChanges = 0;
+int visits = m;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[row, col] = i + 1;
+    if (--visits == 0)
+    {
+        visits = m * (dirChanges % 2) + n * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+        int temp = dx;
+        dx = -dy;
+        dy = temp;
+        dirChanges++;
+    }
+
+    col += dx;
+    row += dy;
+}
+
+for (int rows = 0; rows < array.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < array.GetLength(1); columns++)
+    {
+        Console.Write(array[rows, columns]);
+    }
+    Console.WriteLine();
+}
---
+++ programming-languages/Lecture/Lecture7/Task1/Program.cs
@@ -0,0 +1,93 @@
+﻿// //////////////////////////////////////////////////////////////////////////////
+// Собрать строку с числами от А до B
+//Вариант1
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+//Вариант2
+string NumbersRec1(int a, int b)
+{
+    if (a <= b)
+    {
+        return $"{a} " + NumbersRec1(a + 1, b);
+    }
+    else return String.Empty;
+}
+//Вариант3
+string NumbersRec2(int a, int b)
+{
+    if (a > b)
+    {
+        return String.Empty;
+    }
+    else return $"{a} " + NumbersRec2(a + 1, b);
+}
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec1(1, 10));
+Console.WriteLine(NumbersRec2(1, 10));
+///////////////////////////////////////////////////////////////////////////////////
+// Сумма чисел от 1 до N
+//Вариант1
+int SumFor(int num)
+{
+    int result = 0;
+    for (int i = 0; i <= num; i++) result += i;
+    return result;
+}
+//Вариант2
+int SumRec(int num)
+{
+    if (num == 0) return 0;
+    else return num + SumRec(num - 1);
+}
+Console.WriteLine(SumFor(10));
+Console.WriteLine(SumRec(10));
+/////////////////////////////////////////////////////////////////////////////////////
+// A^n
+//Вариант1
+int PowFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+//Вариант2
+int PowRec(int a, int n)
+{
+
+    return n == 0 ? 1 : PowRec(a, n - 1) * a;
+}
+Console.WriteLine(PowFor(5, 2));
+Console.WriteLine(PowRec(5, 2));
+
+//Вариант3
+int PowRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowRecMath(a * a, n / 2);
+    else return PowRecMath(a, n - 1) * a;
+}
+Console.WriteLine(PowRecMath(5, 2));
+/////////////////////////////////////////////////////////////////////////
+// Собрать все возможные варианты буквенных фраз
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($" {n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("dfgtr",new char[2]);
