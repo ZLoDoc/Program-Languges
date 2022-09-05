3ac158b25e2907f2cbd0debbe02a91380bd40c5a
ZloDoc <zlodoc@rambler.ru>
2022-09-05 16:30:20 +0300

    Test programm

--- Temp/temp1/Program.cs
+++ Temp/temp1/Program.cs
@@ -1,17 +1,33 @@
-﻿
-int[] tempArray = new int[10];
-for (int i = 0; i <= tempArray.Length - 1;i++)
+﻿// char[] symb = { 'A', 'L', 'I', 'S' };
+// int count = 0;
+// for (int i = 0; i < symb.Length; i++)
+// {
+
+//     for (int j = 0; j < symb.Length; j++)
+//     {
+//         for (int k = 0; k < symb.Length; k++)
+//         {
+//             for (int n = 0; n < symb.Length; n++)
+//             {
+//                 count++;
+//                 Console.WriteLine($"{count} {symb[i]}{symb[j]}{symb[k]}{symb[n]}");
+//             }
+//         }
+//     }
+// }
+
+// for (double a=-500; a<500;a++)
+// {
+//     for(double b=-500;b<500;b++)
+//     {
+//         if (2*a*a*a+3*b*b==8337) Console.WriteLine($" a= {a}, b= {b}");
+//     }
+// }
+int count=0;
+for (int i=0; i<=10;i++)
 {
-    tempArray[i] = new Random().Next(0, 5);
-    Console.Write($"{tempArray[i]} \t");
+Console.WriteLine(++count);
 }
 
-Array.Sort(tempArray);
-    Console.WriteLine();
-
-// for (int j = 0; j <= tempArray.Length - 1;j++)
-// {
-//     Console.Write($"{tempArray[j]} \t");
-// }
-Console.Write(Array.IndexOf(tempArray,3));
-
+int[,]array=new int[,]{{3,4,5,6,7,8},{3,4,5,6,7,8},{3,4,5,6,7,8}};
+Console.WriteLine($"memory={array.Length*sizeof(int)}");
---
+++ Test/Program.cs
@@ -0,0 +1,38 @@
+﻿void PrintArray(string[] array)
+{
+    for (int i = 0; i <= array.Length - 1; i++) Console.WriteLine(array[i]);
+    //Console.WriteLine($"array.Lenght={array.Length}");
+}
+string[] ResultArray(string[] array)
+{
+    string[] temporaryArray = new string[array.Length];
+    int temporaryArrayIndex = 0;
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        if (array[i].Length == 3)
+        {
+            temporaryArray[temporaryArrayIndex] = array[i];
+            temporaryArrayIndex++;
+        }
+    }
+    string[] resultArray = new string[temporaryArrayIndex];
+    for (int resultArrayindex = 0; resultArrayindex <= temporaryArrayIndex - 1; resultArrayindex++)
+    {
+        resultArray[resultArrayindex] = temporaryArray[resultArrayindex];
+    }
+    return (resultArray);
+}
+Console.Write("Сколько строк будете вводить? :");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[num];
+for (int i = 0; i <= num - 1; i++)
+{
+    Console.Write($"Введите {i + 1} набор символов :");
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine("Среди введенных наборов символов :");
+PrintArray(array);
+string[] res = ResultArray(array);
+if (res.Length!=0) Console.WriteLine("Присутствуют наборы из 3-х символов :");
+else Console.WriteLine("Hаборы из 3-х символов отсутствуют");
+PrintArray(res);
